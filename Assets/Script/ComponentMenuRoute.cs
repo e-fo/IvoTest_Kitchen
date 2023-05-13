9e540b23e63ec65f59a1bc260b23e6e9889d36ea
Ehsan Foadian <ehsanfoadian@gmail.com>
2023-05-13 10:33:20 +0330

    Player movement was implemented

---
+++ Assets/Script/ComponentMenuRoute.cs
@@ -0,0 +1,9 @@
+namespace IvoKitchen
+{
+    public class ComponentMenuRoute
+    {
+        public const string ROOT="IvoKitchen/";
+        public const string TRANSFORM = "Transform/";
+        public const string RIGIBODY = "Rigidbody/";
+    }
+}
---
+++ Assets/Script/Transform/RigidbodyMoveByDirection.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+
+namespace IvoKitchen
+{
+    using CMRoute = ComponentMenuRoute;
+    [RequireComponent(typeof(Rigidbody))]
+    [AddComponentMenu(CMRoute.ROOT + CMRoute.RIGIBODY + "RigidbodyMoveByDirection")]
+    public class RigidbodyMoveByDirection : MonoBehaviour
+    {
+        [SerializeField] Vector2Variable _moveValue;
+        [SerializeField] FloatVariable _velocity;
+        Rigidbody _rb;
+        Vector3 _dir;
+
+
+        private void OnEnable()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update() 
+        {
+            _dir = new Vector3(_moveValue.Value.x, 0, _moveValue.Value.y).normalized;
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = _dir * _velocity.Value;
+        }
+    }
+}
